8baa8c5e27d98101b74be3823c718680c772d3bf
Breno da Luz Maciel <maciel_lpnm@hotmail.com>
2023-12-14 17:20:06 -0300

    Criado Categoria e tipos de Lanches

--- BrandosLanches/obj/Debug/net6.0/BrandosLanches.AssemblyInfo.cs
+++ BrandosLanches/obj/Debug/net6.0/BrandosLanches.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrandosLanches")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bbab89150232964a5a1ef74a1e57b78a63fbb53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrandosLanches")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrandosLanches")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
